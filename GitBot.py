bf30a8233ca0d0fa94a3e10d64142b02d91730ba
phamlee <leepham@utexas.edu>
2016-04-10 03:40:14 -0500

    timestamp

--- GitBot.py
+++ GitBot.py
@@ -7,7 +7,7 @@ while True:
     f.write(time.strftime('%m-%d-%Y %H:%M:%S')+'\n')
     f.close()
 
-    subprocess.call(“git add -A”)
-    subprocess.call(“git commit -m ‘add timestamp’”)
-    subprocess.call(“git push origin master”)
+    subprocess.call("git add .", shell=True)
+    subprocess.call("git commit -m timestamp", shell=True)
+    subprocess.call("git push origin master", shell=True)
     time.sleep(10)
